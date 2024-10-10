20119dfd5ebdeb886dda7623994913fdf13cb2aa
rapha04 <raphamacalling18@gmail.com>
2024-10-10 16:29:34 +0800

    pushing activity one

--- act_twooo/index.php
+++ act_twooo/index.php
@@ -1,13 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php
-    echo "ongoing"
+<?php
+    $numbah = 1;
+    while ($numbah <= 10){
+        echo $numbah;
+        $numbah++;
+    }
+    echo "<br>";
+    $numb = 2;
+    while ($numb <= 20){
+        echo $numb . " ";
+        $numb +=2;
+    }
+    
     ?>
-</body>
-</html>
